9460e09392e8e70089d12df49ae0298ebd82b654
CronosJoe <CronosJoe@gmail.com>
2021-10-08 17:40:47 -0700

    Finally am putting project on git

---
+++ 3DPlatformer/Assets/Scripts/Buttons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public void OnQuitButton() 
+    {
+        //do any clean up needed here
+        Application.Quit();
+    }
+    public void OnPlayButton() 
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void OnSettingsButton() 
+    {
+    //load up whatever settings I want
+    }
+}
---
+++ 3DPlatformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Terry's motor so we can use it
+    public KinematicPlayerMotor motor;
+    public PlayerInput input;
+    bool isTeleportable = false;
+    //implement animations here or in another script
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        input.currentActionMap["Jumping"].performed += jumpPlayer;
+        input.currentActionMap["Teleport"].performed += teleportPlayer;
+    }
+    private void OnDisable()
+    {
+        try
+        {
+            input.currentActionMap["Jumping"].performed -= jumpPlayer;
+        }
+        catch(NullReferenceException)
+        {
+            Debug.Log("Failed to unsubscribe from the jumpPlayer method due to input manager being removed first");
+        }
+        
+    }
+    private void teleportPlayer(InputAction.CallbackContext obj)
+    {
+        if (isTeleportable) 
+        {
+
+        }
+    }
+    private void jumpPlayer(InputAction.CallbackContext obj) //this will be called when the player hits the space bar or whatever binding I give to jump and simply apply the jump vector in terry's code
+    {
+        motor.JumpInput();
+    }
+    private void Update()//we'll use update to handle our actual movement
+    {
+        Vector2 movementVec2 = input.currentActionMap["Walking"].ReadValue<Vector2>();
+        motor.MoveInput(new Vector3(movementVec2.x, 0.0f, movementVec2.y));
+
+        //check the teleport spot and place the special effects
+    }
+
+    private void CheckTeleport() 
+    {
+        
+    }
+
+}
